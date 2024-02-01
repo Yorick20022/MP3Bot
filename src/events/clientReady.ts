e0b6d2833dedd248ab52b6615d82e396ca9ad190
Yorick2002 <125297057+Yorick20022@users.noreply.github.com>
2024-02-01 14:20:58 +0100

    did more thing

--- src/events/clientReady.ts
+++ src/events/clientReady.ts
@@ -5,22 +5,18 @@ import path from 'path'
 export const name = Events.ClientReady;
 export const once = true;
 export async function execute(client: Client, guild: any) {
-	const embed = new EmbedBuilder()
-		.setTitle('Ready!')
-		.setDescription('Logged in as ' + client.user!.tag)
-		.setColor('#443221');
-	const logChannel = client.channels.cache.get("1159220242464116886");
 	console.log(`Ready!\nLogged in as ${client.user!.tag}!`);
 	client.user!.setPresence({ activities: [{ name: 'Javascript' }] });
 
 	const dirName = path.resolve(__dirname, '..', '..', "src", "audio")
 	console.log(dirName);
 
+	// Create dir if it does not exist
 	try {
 		if (!fs.existsSync(dirName)) {
 			fs.mkdirSync(dirName)
 		}
-
+		// If dir is not empty, empty it, else do nothing.
 		for (const file of await fs.promises.readdir(dirName)) {
 			if (file.length == 0) continue
 			fs.unlink(path.join(dirName, file), (err) => {
